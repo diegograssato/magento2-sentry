b239f0d83cf819266ee7085a454b8918dcd826e4
Diego Pereira Grassato <diego.grassato@gmail.com>
2018-09-27 22:44:37 -0300

    FIx namespace

--- Sentry/Client.php
+++ Sentry/Client.php
@@ -16,37 +16,32 @@ class Client extends Raven_Client
     public function __construct()
     {
         $ravenDNS = null;
-        $options  = [ ];
+        $options = [];
 
-        if ( php_sapi_name() !== 'cli' )
-        {
-            $root = __DIR__.'/../../../..';
+        if (php_sapi_name() !== 'cli') {
+            $root = __DIR__ . '/../../../..';
 
-            if ( is_file($envFile = $root . '/app/etc/env.php') )
-            {
+            if (is_file($envFile = $root . '/app/etc/env.php')) {
                 $env = include $envFile;
-            }
-            else
-            {
+            } else {
                 throw new \Exception('Cannot read env file');
             }
 
-            $isDeveloper = array_key_exists('MAGE_MODE', $env) && $env[ 'MAGE_MODE' ] === 'developer';
+            $isDeveloper = array_key_exists('MAGE_MODE', $env) && $env['MAGE_MODE'] === 'developer';
 
             // Only log to Sentry if the use is not in development mode
-            if ( !$isDeveloper )
-            {
-                $ravenDNS = array_key_exists('raven_dns', $env) ? $env[ 'raven_dns' ] : null;
+            if (!$isDeveloper) {
+                $ravenDNS = array_key_exists('raven_dns', $env) ? $env['raven_dns'] : null;
             }
         }
-        $this->setEnvironment($env[ 'MAGE_MODE' ]);
+        $this->setEnvironment($env['MAGE_MODE']);
         exec('git describe --always', $version_mini_hash);
         exec('git describe --tags --abbrev=0', $tag);
 
         $this->tags_context([
-            'php_version'   => phpversion(),
-            'git_commit'    => $version_mini_hash,
-            'git_tag'       => $tag
+            'php_version' => phpversion(),
+            'git_commit' => $version_mini_hash,
+            'git_tag' => $tag
         ]);
 
         parent::__construct($ravenDNS, $options);
--- Sentry/Handler.php
+++ Sentry/Handler.php
@@ -8,15 +8,14 @@ class Handler extends \Raven_ErrorHandler
 {
     /**
      * Handler constructor.
-     * @param $ravenClient \violuke\Sentry\Raven\Client
+     * @param $ravenClient \DTuX\Sentry\Client
      * @param $level
      * @param bool $bubble
      */
     public function __construct($ravenClient, $level = Logger::DEBUG, $bubble = true)
     {
 
-        if ( is_array($ravenClient) && array_key_exists('instance', $ravenClient) )
-        {
+        if (is_array($ravenClient) && array_key_exists('instance', $ravenClient)) {
             $ravenClient = new $ravenClient['instance'];
         }
         $client->setRelease($tag);
@@ -34,8 +33,9 @@ class Handler extends \Raven_ErrorHandler
      * @param str $branch The git branch to check
      * @return mixed Either the hash or a boolean false
      */
-    function get_current_git_commit( $branch='master' ) {
-        if ( $hash = file_get_contents( sprintf( '.git/refs/heads/%s', $branch ) ) ) {
+    function get_current_git_commit($branch = 'master')
+    {
+        if ($hash = file_get_contents(sprintf('.git/refs/heads/%s', $branch))) {
             return $hash;
         } else {
             return false;
