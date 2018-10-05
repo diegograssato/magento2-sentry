5e381d20af00fff0eebc6d10a86b393d47fa0446
Diego Pereira Grassato <dgrassato@ciandt.com>
2018-10-05 19:37:13 +0000

    Change namespace and fix PSR4

--- Sentry/Handler.php
+++ Sentry/Handler.php
@@ -1,29 +1,32 @@
 <?php
 
-namespace DTuX\Sentry;
+namespace DTuX\Sentry\Sentry;
 
-use Monolog\Logger;
+use \Psr\Log\LogLevel;
+use Monolog\Handler\RavenHandler;
 
-class Handler extends \Raven_ErrorHandler
+class Handler extends RavenHandler
 {
     /**
      * Handler constructor.
-     * @param $ravenClient \DTuX\Sentry\Client
+     * @param $ravenClient \DTuX\Sentry\Sentry\Client
      * @param $level
      * @param bool $bubble
      */
-    public function __construct($ravenClient, $level = Logger::DEBUG, $bubble = true)
+    public function __construct($ravenClient, $level = LogLevel::DEBUG, $bubble = true)
     {
 
         if (is_array($ravenClient) && array_key_exists('instance', $ravenClient)) {
+
             $ravenClient = new $ravenClient['instance'];
         }
 
-        parent::__construct($ravenClient, $level, $bubble);
+        $this->setFormatter(new \Monolog\Formatter\LineFormatter("%message% %context% %extra%\n"));
 
-//        $this->registerExceptionHandler();
-//        $this->registerErrorHandler();
-//        $this->registerShutdownFunction();
+        $ravenClient->setRelease('000000000001111111111122');
+        $ravenClient->setEnvironment(getenv('MAGENTO_RUN_MODE'));
+
+        parent::__construct($ravenClient, $level, $bubble);
     }
 
 }
